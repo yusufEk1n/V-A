7ed3c8c499cbce0c9d248f4766bf51580103ce47
yusufEk1n <ekinn.yusuf56@outlook.com>
2023-12-24 07:01:33 +0300

    mobile app initialized

---
+++ src/mobile/app/src/main/java/com/example/virtualassistant/assistant/ui/VoiceActivationFragment.kt
@@ -0,0 +1,4 @@
+package com.example.virtualassistant.assistant.ui
+
+class VoiceActivationFragment {
+}
---
+++ src/mobile/app/src/main/java/com/example/virtualassistant/assistant/util/ResponseParser.kt
@@ -0,0 +1,4 @@
+package com.example.virtualassistant.assistant.util
+
+class ResponseParser {
+}
